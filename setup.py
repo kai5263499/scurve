80e4857b79fae6045f07bcc98430f2ee3e8cb409
Joe Blow <joe@Joes-MacBook-Pro.local>
2016-01-27 08:38:25 -0500

    update image library dependency to make this package more portable

--- setup.py
+++ setup.py
@@ -1,4 +1,27 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+import pkg_resources
+
+install_requires = []
+
+def package_installed(pkg):
+    """Check if package is installed"""
+    req = pkg_resources.Requirement.parse(pkg)
+    try:
+        pkg_resources.get_provider(req)
+    except pkg_resources.DistributionNotFound:
+        return False
+    else:
+        return True
+
+# depend in Pillow if it is installed, otherwise
+# depend on PIL if it is installed, otherwise
+# require Pillow
+if package_installed('Pillow'):
+    install_requires.append('Pillow !=2.4.0')
+elif package_installed('PIL'):
+    install_requires.append('PIL>=1.1.6,<1.2.99')
+else:
+    install_requires.append('Pillow !=2.4.0')
 
 version = "0.2"
 setup(
@@ -9,4 +32,5 @@ setup(
         author_email = "aldo@corte.si",
         packages = ["scurve"],
         scripts = ["colormap", "cube", "drawcurve", "gray", "testpattern", "binvis"],
+        install_requires = install_requires
 )
